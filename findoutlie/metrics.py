6b18fb8d0a7570a720614c630ddb040d7dc0f034
Felipe Cybis Pereira <felipe.cybis-pereira@espci.psl.eu>
2022-09-06 10:54:59 +0200

    Added dvars metrics * dvars test now is passing as well

--- findoutlie/metrics.py
+++ findoutlie/metrics.py
@@ -1,8 +1,6 @@
 """ Scan outlier metrics
 """
-
-# Any imports you need
-# +++your code here+++
+import numpy as np
 
 
 def dvars(img):
@@ -21,12 +19,6 @@ def dvars(img):
         One-dimensional array with n-1 elements, where n is the number of
         volumes in `img`.
     """
-    # Hint: remember 'axis='.  For example:
-    # In [2]: arr = np.array([[2, 3, 4], [5, 6, 7]])
-    # In [3]: np.mean(arr, axis=1)
-    # Out[2]: array([3., 6.])
-    #
-    # You may be be able to solve this in four lines, without a loop.
-    # But solve it any way you can.
-    # This is a placeholder, replace it to write your solution.
-    raise NotImplementedError('Code up this function')
+    data = img.get_fdata().reshape(-1, img.shape[-1])
+    data_diff_squared = np.diff(data) ** 2
+    return np.sqrt(np.mean(data_diff_squared, axis=0))
